bbf1293302eeeeada73a0c6a456c53f5657ac478
Flávio Rocha <flavio-10_28@hotmail.com>
2025-02-19 14:55:15 -0300

    Criação inicial testes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { HeaderComponent } from './components/header/header.component';
-import { TodoCardComponent } from "./components/todo-card/todo-card.component";
+import { TodoCardComponent } from './components/todo-card/todo-card.component';
 
 @Component({
   selector: 'app-root',
@@ -12,5 +12,13 @@ import { TodoCardComponent } from "./components/todo-card/todo-card.component";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @Input() public projectName!: string;
+  @Output() public outputEvent = new EventEmitter<string>();
+
   title = 'todo-list';
+
+  public handleEmmitEvent(): void {
+    this.outputEvent.emit(this.projectName);
+  }
+
 }
