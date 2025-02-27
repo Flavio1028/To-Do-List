f533c9ca0f390f8c69562a124167fea75751916f
Flávio Rocha <flavio-10_28@hotmail.com>
2025-02-27 11:04:20 -0300

    Criação teste pip

---
+++ src/app/shared/pipes/custom-upper-case.pipe.spec.ts
@@ -0,0 +1,17 @@
+import { CustomUpperCasePipe } from './custom-upper-case.pipe';
+
+describe('CustomUpperCasePipe', () => {
+
+  const customUpperCasePipe = new CustomUpperCasePipe();
+
+  it('should return ANGULAR TEST ', () => {
+    const textTransformed = customUpperCasePipe.transform('angular test');
+    expect(textTransformed).toEqual('ANGULAR TEST');
+  });
+
+  it('should return ANGULAR 16 ', () => {
+    expect(customUpperCasePipe.transform('ANGULAR 16')).toEqual('ANGULAR 16');
+  });
+
+
+});
